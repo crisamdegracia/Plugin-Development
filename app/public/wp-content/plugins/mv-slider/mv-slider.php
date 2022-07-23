a80108413f7efc6d45e043b2271d6802041f74de
crisam d egracia <crisamdegracia@gmail.com>
2022-07-24 00:13:14 +0800

    shat

---
+++ app/public/wp-content/plugins/mv-slider/mv-slider.php
@@ -0,0 +1,10 @@
+<?php
+/* 
+* Plugin Name: MV Slider
+* Plugin URI: sample.uri.com
+* Description: Test Description Lorem ipsum
+* Version: 1.0
+* Requires at least: 5.6
+* Author: Crisam Pogee
+* Author
+*/
